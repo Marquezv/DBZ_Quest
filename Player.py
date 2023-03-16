af8ffb9d5e2dc4803e11c728ddb7b3e1e8694f7f
Marquezv <vmarquezv@outlook.com.br>
2023-03-16 12:49:00 -0300

    feature: add Radar

--- Player.py
+++ Player.py
@@ -25,7 +25,7 @@ class Player():
         self.rect = pygame.Rect(self.x, self.y, self.height, self.width)
         history(self.sc, self.rect, ht)
         pygame.draw.rect(self.sc, self.color, self.rect) 
-
+        radar(self.sc, self.rect)
         if key[pygame.K_LCTRL] and key[pygame.K_s]:
             clean(self.sc, ht[1])
           
@@ -38,8 +38,9 @@ def clean(sc, block):
     pygame.draw.rect(sc, ht_color, rect)
 
 def history(sc, block, ht):
-
-    color = sc.get_at((block.x, block.y))
+    x = block.x + 10
+    y = block.y + 10
+    color = sc.get_at((x, y))
     block_data = {
             'x': block.x,
             'y': block.y,
@@ -55,4 +56,11 @@ def history(sc, block, ht):
     ht_color = ht[0].get('color')
     rect = pygame.Rect(ht[0].get('x'), ht[0].get('y'), ht[0].get('height'), ht[0].get('width'))
     pygame.draw.rect(sc, ht_color, rect)
-    
+
+
+def radar(sc, player):
+    x = player.x - 63
+    y = player.y - 63
+    rect = pygame.Rect(x, y, 147, 147)
+    pygame.draw.rect(sc, (255, 0, 0, 255), rect, 1)
+
